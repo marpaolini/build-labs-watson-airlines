7bd3f66ae89e68509925c3c5b63cf6bd719f98ea
Mariana Paolini <m.paolinilara@gmail.com>
2023-07-11 19:59:54 -0300

    actualizacion app.js

---
+++ source/back-end/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const {router} = require ('./sample/routes/routes.js')
+
+const {create_connection} = require('./sample/sample.mongodb');
+
+const app = express();
+const port = 3001;
+
+app.use(express.static(path.resolve('/Users/mpaol/OneDrive/Documentos/build-labs-watson-airlines/source/front-end')));
+
+app.use(router)
+
+app.listen(port, () => {
+    console.log(`Server in http://localstorage:${port}/`);
+});
+create_connection().then(() => console.log("successful connection"));
